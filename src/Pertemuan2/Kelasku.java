96a40dc347a69163746bdc34179329831ccc69de
acer <acer@LAPTOP-ECI5I3DL>
2023-03-11 11:46:43 +0700

    Pertemuan 2

---
+++ src/Pertemuan2/Kelasku.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pertemuan2;
+
+/**
+ *
+ * @author acer
+ */
+public class Kelasku {
+    
+    public static void main(String[] args){
+        Person objKu= new Person();
+//        objKu.name = "Frenty";
+          objKu.setName("Frenty");
+//        System.out.println("Nama : "+objKu.name);
+          System.out.println("Nama : "+objKu.getName());
+    }   
+}
